c2bf04e55cda8e0d26ad2f1ec359fe739acaeff5
averner <eliador12@gmail.com>
2020-06-17 16:07:01 +0700

    Add DocumentGenerationUtil

---
+++ Tools/TemplateGenerationUtil/TemplateGenerationUtil.Core/TemplateGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateGenerationUtil.Core
+{
+    public class TemplateGenerator
+    {
+        public void Generate(Solution solution)
+        { 
+        }
+    }
+}
---
+++ Tools/TemplateGenerationUtil/TemplateGenerationUtil.Standalone/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+using TemplateGenerationUtil.Core;
+
+namespace TemplateGenerationUtil.Standalone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MSBuildLocator.RegisterDefaults();
+            using (var workspace = MSBuildWorkspace.Create())
+            {
+                var solution = workspace.OpenSolutionAsync(args[0]).Result;
+
+                var templateGenerator = new TemplateGenerator();
+                templateGenerator.Generate(solution);
+            }
+        }
+    }
+}
