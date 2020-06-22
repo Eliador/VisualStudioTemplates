f0742a206306d46b7874504be3e83884e8dafb3e
averner <eliador12@gmail.com>
2020-06-22 14:47:21 +0700

    Create MonolithicWebAppWizardizard for MonolithicWebApp template

---
+++ MonolithWebApp/MonolithicWebApp.Wizard/TemplateWizard.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TemplateWizard;
+using System.Collections.Generic;
+
+namespace MonolithicWebApp.Wizard
+{
+    public class TemplateWizard : IWizard
+    {
+        private static string basename;
+
+        public void BeforeOpeningFile(global::EnvDTE.ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(global::EnvDTE.Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(global::EnvDTE.ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            if (runKind == WizardRunKind.AsMultiProject)
+            {
+                basename = replacementsDictionary["$specifiedsolutionname$"];
+            }
+            else
+            {
+                replacementsDictionary["$safeprojectname$"] = basename;
+            }
+        }
+    }
+}
